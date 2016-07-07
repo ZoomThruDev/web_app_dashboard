b470d6850e4235a4d027c900495fd9a63c942a40
Ryan Lijewski <RLijewski@users.noreply.github.com>
2016-07-06 19:54:33 -0500

    Added line graph, bar graph, and pie chart

--- js/main.js
+++ js/main.js
@@ -1,37 +1,131 @@
-var ctx = document.getElementById("web-traffic");
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+var lineGraph = document.getElementById("line-graph").getContext("2d");
+var barGraph = document.getElementById("bar-graph").getContext("2d");
+var pieChart = document.getElementById("pie-chart").getContext("2d");
+
+// ************
+// Line Graph *
+// ************
+
+var webTrafficData = {
+    labels: ["", "16 - 22", "23 - 29", "30 - 5", "6 - 12", "13 - 19", "20 - 26", "27 - 3", "4 - 10", "11 - 17", "18 - 24", "25 - 31"],
+    datasets: [
+        {
+            label: "Weekly web Traffic",
+            data: [0, 500, 1000, 750, 1250, 750, 1500, 1000, 1500, 2000, 1500, 2000],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(116, 119, 191, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(116, 119, 191, 1)'
             ],
-            borderWidth: 1
-        }]
-    },
+            pointBorderColor: "rgba(116, 119, 191, 1)",
+            pointBackgroundColor: "#fff",
+            pointRadius: 5,
+            borderWidth: 1,
+            lineTension: 0
+        }
+    ]
+};
+
+var webTrafficChart = new Chart(lineGraph, {
+    type: "line",
+    data: webTrafficData,
     options: {
+        responsive: true,
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero:true
+                    beginAtZero:true,
+                    min: 0,
+                    max: 2500,
+                    stepSize: 500
                 }
             }]
         }
     }
 });
+
+
+
+// ***********
+// Bar Graph *
+// ***********
+
+var dailyTrafficData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [
+        {
+            label: "Daily Traffic",
+            backgroundColor: 'rgba(116, 119, 191, 1)',
+            borderColor: 'rgba(116, 119, 191, 1)',
+            borderWidth: 1,
+            data: [50, 75, 150, 100, 200, 175, 75]
+        }
+    ]
+}
+
+var dailyTrafficChart = new Chart(barGraph, {
+    type: 'bar',
+    data: dailyTrafficData,
+    options: {
+        responsive: true,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true,
+                    min: 0,
+                    max: 250,
+                    stepSize: 50
+                }
+            }]
+        }
+    }
+});
+
+
+
+// ***********
+// Pie Chart *
+// ***********
+
+var mobileUsersData = {
+    labels: [
+        "Red",
+        "Blue",
+        "Yellow"
+    ],
+    datasets: [
+        {
+            data: [300, 50, 100],
+            backgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ],
+            hoverBackgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ]
+        }]
+}
+
+var mobileUsersChart = new Chart(pieChart, {
+    type: 'doughnut',
+    data: mobileUsersData
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
